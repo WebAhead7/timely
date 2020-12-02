dd6861110eb48afe9ec6c49a452f36bf958aefe8
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2020-12-02 18:57:23 +0200

    ff

--- handlers/doctorHandlers/signup.js
+++ handlers/doctorHandlers/signup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const model = require("../../database/model");
 
 function doctorSignUp(req, res, next) {
@@ -6,48 +5,17 @@ function doctorSignUp(req, res, next) {
   model
     .getDoctorsEmail()
     .then((emails) => {
-      console.log("11111", emails);
       const emailsArr = emails.map((elem) => elem.email);
       const Valied = emailsArr.some((elem) => elem === doctor.email);
 
-      console.log("kkkkk", Valied);
       if (!Valied) {
-        console.log("im in valid");
         model.doctorSignUp(doctor).catch(next);
-        res.status(200).send("done");
+        res.status(200).send({ msg: "done", auth: true });
       } else {
-        res.status(404).send({ msg: "Invalied Email" });
+        res.status(404).send({ msg: "Invalied Email", auth: false });
       }
     })
     .catch(next);
-=======
-
-
-const  model = require("../../database/model");
-
-
-
-function doctorSignUp(req,res,next){
-    const doctor = req.body; 
-    model.getDoctorsEmail().then(emails=> { 
-        const emailsArr = emails.map(elem => elem.email);
-        const Valied = emailsArr.some(elem=> elem === doctor.email);
-        
-        if(!Valied){
-            model.doctorSignUp(doctor).catch(next);
-            res.status(200).send({msg:"done", auth:true});
-            
-        }
-
-        else {
-        
-        res.status(404).send({msg: "Invalied Email", auth:false});
-    }
-
-    }).catch(next);
-
-
->>>>>>> server_updates_doctor
 }
 
 module.exports = doctorSignUp;
