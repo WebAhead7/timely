2ccb9886b8d045dcb5050af2f919fc3909cc185d
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2020-12-02 19:07:05 +0200

    new changes

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectionString =
-  process.env.DATABASE_URL ||
   "postgres://alaabashiyi:321321@localhost:5432/timely_db";
 
 const db = new pg.Pool({ connectionString });
